d6ed4b5e8413380a95dd97d9f33918af854de258
albert kyrios <albertsardi@gmail.com>
2024-08-22 11:19:19 +0700

    add header

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -7,17 +7,17 @@
 <link rel="stylesheet" href="http://localhost/lav11_invplanePdf/public/assets/css/bootstrap.min.css">
 <!-- <link rel="stylesheet" href="{{url('/assets/css/bootstrap.min.css') }}"> -->
 
-<link rel="stylesheet" href="http://localhost/lav11_invplanePdf/public/assets/css/style.css?v=1.6.1">
-<link rel="stylesheet" href="{{url('/assets/css/style.css?v=1.6.1') }}">
-<link rel="stylesheet" href="{{url('/assets/css/custom.css?v=1.6.1') }}">
-<link rel="stylesheet" href="{{url('/assets/css/fontawesome/font-awesome.min.css') }}">
+<link rel="stylesheet" href="{{asset('/assets/css/style.css?v=1.6.1') }}">
+<link rel="stylesheet" href="{{asset('/assets/css/style.css?v=1.6.1') }}">
+<link rel="stylesheet" href="{{asset('/assets/css/custom.css?v=1.6.1') }}">
+<link rel="stylesheet" href="{{asset('/assets/css/fontawesome/font-awesome.min.css') }}">
 
 <link rel="stylesheet" href="{{ asset('/assets/css/style.css?v=1.6.1') }}">
 <!-- <link rel="stylesheet" href="http://localhost/lav9Invplane/assets/css/style.css?v=1.6.1"> -->
 <link href="{{ asset('storage/assets/css/fontawesome/font-awesome.min.css') }}" rel="stylesheet" type="text/css" >
 <!-- <link rel="icon" type="image/png" href="{{ asset('/assets/images/favicon.ico') }}"> -->
 
-    <link rel="stylesheet" href="{{url('/assets/css/monospace.css?v=1.6.1') }}">
+    <link rel="stylesheet" href="{{asset('/assets/css/monospace.css?v=1.6.1') }}">
 
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/bootbox.js/6.0.0/bootbox.js"></script>
 
