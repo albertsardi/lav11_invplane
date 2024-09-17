9ed5898c1a8fec55f8f1267cb8f1bd62c85b1617
albert <albertsardi@gmail.com>
2024-09-17 15:37:55 +0700

    add quotation row edit form

---
+++ resources/views/components/modal/quotation_rowEdit.blade.php
@@ -0,0 +1,34 @@
+<div id='modal-quotation-row-edit'>
+    <form>
+        <div class="mb-3">
+            <label for="" class="form-label">id</label>
+            <input name="id" type="text" class="form-control" readonly value=''>
+        </div>    
+        <div class="mb-3">
+            <label for="exampleInputEmail1" class="form-label">Product</label>
+            <input name="ProductCode" type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+            <div id="emailHelp" class="form-text">Choose at least 1 product</div>
+        </div>
+        <div class="mb-3">
+            <label for="" class="form-label">Qty</label>
+            <input name="Qty" type="text" class="form-control">
+        </div>
+        <div class="mb-3">
+            <label for="" class="form-label">Price</label>
+            <input name="Price" type="text" class="form-control">
+        </div>
+        <div class="mb-3">
+            <label for="" class="form-label">Amount</label>
+            <input name="Amount" type="text" class="form-control" readonly value=0>
+        </div>
+        <div class="mb-3">
+            <label for="exampleInputPassword1" class="form-label">UNit Cost</label>
+            <input name='Cost' type="text" class="form-control" readonly value=0>
+        </div>
+        <div class="mb-3 form-check">
+            <input type="checkbox" class="form-check-input" id="exampleCheck1">
+            <label class="form-check-label" for="exampleCheck1">Check me out</label>
+        </div>
+        <button type="submit" class="btn btn-primary">Submit</button>
+    </form>
+</div>
