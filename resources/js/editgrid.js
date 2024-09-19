1145af2f46b2942bdbb1b8c07be84ffd62b60e08
albert <albertsardi@gmail.com>
2024-09-19 14:37:42 +0700

    add jquery vers 3.7.1

---
+++ resources/js/editgrid.js
@@ -0,0 +1,9 @@
+function xrow_edit(id) {
+        //alert('row2 edit '+id);
+        //id = id??'0';
+        bootbox.alert('row2 edit '+id??'0');
+    }
+    function row_delete(id) {
+        //alert('row2 delete '+id);
+        bootbox.alert('row delete '+id??'0');
+    }
